a5b4a1bcf5f63be7ab39b336825c65daa019912b
jb <2533012037@qq.com>
2021-02-07 15:03:53 +0800

    test

---
+++ src/com/test/domain.java
@@ -0,0 +1,31 @@
+package com.test;
+
+public class domain {
+    private String id;
+    private String name;
+
+    @Override
+    public String toString() {
+        return "domain{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
